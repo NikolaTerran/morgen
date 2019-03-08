512e29dcddff4ced92354facdd5360eb790a98c6
Nikola Terran <nikolaterranthe1@gmail.com>
2019-03-08 11:46:43 -0500

    s

---
+++ curve.c
@@ -0,0 +1,7 @@
+#include "engine.h"
+
+struct Matrix bezier(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4){
+	struct Matrix curve;
+	curve = mx_init(4,2);
+	
+}
